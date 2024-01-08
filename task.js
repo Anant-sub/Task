9019a077a34edbadca8efa55cdcbfc424f4604e4
Anant <anant832003@gmail.com>
2024-01-08 13:21:27 +0530

    First commit

---
+++ task.js
@@ -0,0 +1,30 @@
+const fs =  require("fs");
+fs.readFile("input.txt", (err,data) => {
+    if(err){
+        console.err(err);
+    }
+    var answer = 0;
+    const data_str = data.toString();
+    const arr = data_str.split('\n');
+    for(var i = 0; i < arr.length; i++){
+    const cur = arr[i];
+    // console.log(cur);
+    var num = "";
+    for(var j = 0; j < cur.length; j++){
+        if(cur[j] >= '0' && cur[j] <= '9'){
+            num += cur[j];
+            break;
+        }
+    }
+    for(var j = cur.length-1; j >= 0; j--){
+        if(cur[j] >= '0' && cur[j] <= '9'){
+            num += cur[j];
+            break;
+        }
+    }
+    answer += Number(num);
+    }
+    console.log(answer);
+});
+// console.log(arr);
+// var answer = 0;
