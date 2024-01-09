9ab47befc389b35f47bfea705fb7de14b8686794
Anant <anant832003@gmail.com>
2024-01-09 19:17:25 +0530

    Added function to perform the task

--- task.js
+++ task.js
@@ -1,4 +1,28 @@
 const fs =  require("fs");
+
+
+
+function helper(arr){
+    var answer = 0;
+    for(var i = 0; i < arr.length; i++){
+        const cur = arr[i];
+        var num = "";
+        for(var j = 0; j < cur.length; j++){
+            if(cur[j] >= '0' && cur[j] <= '9'){
+                num += cur[j];
+                break;
+            }
+        }
+        for(var j = cur.length-1; j >= 0; j--){
+            if(cur[j] >= '0' && cur[j] <= '9'){
+                num += cur[j];
+                break;
+            }
+        }
+        answer += Number(num);
+        }
+    return answer;
+}
 fs.readFile("input.txt", (err,data) => {
     if(err){
         console.err(err);
@@ -6,22 +30,5 @@ fs.readFile("input.txt", (err,data) => {
     var answer = 0;
     const data_str = data.toString();
     const arr = data_str.split('\n');
-    for(var i = 0; i < arr.length; i++){
-    const cur = arr[i];
-    var num = "";
-    for(var j = 0; j < cur.length; j++){
-        if(cur[j] >= '0' && cur[j] <= '9'){
-            num += cur[j];
-            break;
-        }
-    }
-    for(var j = cur.length-1; j >= 0; j--){
-        if(cur[j] >= '0' && cur[j] <= '9'){
-            num += cur[j];
-            break;
-        }
-    }
-    answer += Number(num);
-    }
-    console.log(answer);
+    console.log(helper(arr));
 });
